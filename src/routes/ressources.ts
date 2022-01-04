e4c595428b4414f61a74d4592a8142bb65483e45
StrixSC <talon.strix@gmail.com>
2022-01-03 22:44:40 -0500

    Added about page content

--- src/routes/ressources.ts
+++ src/routes/ressources.ts
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/ressources", (req, res, next) => {
-    res.render('ressources');
+    res.redirect('https://clist.by/resources/');
 });
 
 export default router;
