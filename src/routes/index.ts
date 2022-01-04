54e0ee7ea781be2931ec6391b3e0cd2950161300
StrixSC <talon.strix@gmail.com>
2022-01-04 05:37:03 -0500

    Added support for injecting Calendar iframe source directly from environment variables

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
 const router = express.Router();
-const calendarSrc = process.env.CALENDAR_SRC
+let calendarSrc = process.env.CALENDAR_SRC
+
+if (!calendarSrc) {
+    console.error("Calendar Source not found...");
+    process.exit(1);
+} else {
+    calendarSrc = Buffer.from(calendarSrc, "base64").toString();
+}
 
 router.get("/", (req, res, next) => {
     res.render('index', { calendarSrc });
